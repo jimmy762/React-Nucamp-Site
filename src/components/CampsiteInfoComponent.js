9d09de214ce0b15d70548dd08dc3f50c0abce436
Sam <samuel.bouk@gmail.com>
2022-04-09 08:04:43 -0800

    add key to CampsiteInfoComponent.js

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ class CampsiteInfo extends Component {
                 <div className='col-md-5 m-1'>
                     <h4>Comments</h4>
                     {comments.map(comment => {return(
-                       <div>
+                       <div key={comment.id}>
                         <p>{comment.text}</p>
                         <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                        </div>
